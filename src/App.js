f7677c74c92debcd63bffecb558321adb8eda852
Anna Ciosek <annaciosek@gmail.com>
2023-03-18 23:26:08 +0000

    add styles and header default props & prop types

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div className="container">
+      <Header />
         <h1>My App</h1>
       </div>
-      <Header />
+      
     </>
   );
 }
